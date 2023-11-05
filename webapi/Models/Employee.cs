de25b7181905add6e2cb035c48f48238e53abaca
Teodor Dichev <teodichev@gmail.com>
2023-11-05 17:12:10 +0200

    Updated database

--- webapi/Models/Employee.cs
+++ webapi/Models/Employee.cs
@@ -10,11 +10,11 @@ namespace webapi.Models
         public required virtual ApplicationUser Profile { get; set; }
         public required string City { get; set; }
         public required DateTime BirthDate { get; set; }
-        public decimal SpeedAverageRating { get; set; }
-        public decimal AttitudeAverageRating { get; set; }
-        public decimal PunctualityAverageRating { get; set; }
-        public decimal CollegialityAverageRating { get; set; }
-        public decimal EmployeeAverageRating { get; set; }
+        public decimal? SpeedAverageRating { get; set; }
+        public decimal? AttitudeAverageRating { get; set; }
+        public decimal? PunctualityAverageRating { get; set; }
+        public decimal? CollegialityAverageRating { get; set; }
+        public decimal? EmployeeAverageRating { get; set; }
         public bool IsLookingForJob { get; set; } = true;
         public virtual ICollection<EmployeeRestaurant> EmployeesRestaurants { get; set; }
         public virtual ICollection<Transfer> Transfers { get; set; }
