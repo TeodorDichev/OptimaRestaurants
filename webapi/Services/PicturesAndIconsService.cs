5a49215645fb44ca1155bd93cee33baaa79f47e4
Teodor Dichev <teodichev@gmail.com>
2023-11-21 09:20:31 +0200

    Added the images path to secrets.json

--- webapi/Services/PicturesAndIconsService.cs
+++ webapi/Services/PicturesAndIconsService.cs
@@ -2,34 +2,40 @@
 {
     public class PicturesAndIconsService
     {
+
+        private readonly IConfiguration _configuration;
+        public PicturesAndIconsService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public string SaveImage(IFormFile imageFile)
         {
-            string fileName = "";
+            string url = "";
 #pragma warning disable CS0168 // Variable is declared but never used
             try
             {
-                var path = Path.Combine(Directory.GetCurrentDirectory(), "D:\\Repos\\OptimaRestaurant\\angularapp\\src\\assets\\uploads");
-                var returnPath = Path.Combine("../../../../assets/uploads");
+                var path = Path.Combine(Directory.GetCurrentDirectory(), _configuration["Images:Path"] ?? string.Empty);
                 if (!Directory.Exists(path)) Directory.CreateDirectory(path);
 
                 var ext = Path.GetExtension(imageFile.FileName);
                 var allowedExtensions = new string[] { ".jpg", ".png", ".jpeg" };
-                if (!allowedExtensions.Contains(ext)) return fileName;
+                if (!allowedExtensions.Contains(ext)) return url;
 
                 string uniqueString = Guid.NewGuid().ToString();
                 var newFileName = uniqueString + ext;
 
                 var fileWithPath = Path.Combine(path, newFileName);
-                var returnFileWithPath = returnPath + $"/{newFileName}";
+                url = "../../../../assets/uploads" + $"/{newFileName}";
                 var stream = new FileStream(fileWithPath, FileMode.Create);
                 imageFile.CopyTo(stream);
                 stream.Close();
 
-                return returnFileWithPath;
+                return url;
             }
             catch (Exception ex)
             {
-                return fileName;
+                return url;
             }
 #pragma warning restore CS0168 // Variable is declared but never used
         }
