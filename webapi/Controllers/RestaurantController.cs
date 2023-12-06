8b6182ada0be436be7cf6c4aa10424240cdecd6a
Teodor Dichev <teodichev@gmail.com>
2023-12-06 21:30:37 +0200

    Fixed sending request logic

--- webapi/Controllers/RestaurantController.cs
+++ webapi/Controllers/RestaurantController.cs
@@ -222,7 +222,7 @@ namespace webapi.Controllers
             var employeeProfile = await _context.Users.FirstOrDefaultAsync(p => p.Email == requestDto.EmployeeEmail);
             if (employee == null || employeeProfile == null) return BadRequest("Потребителят не съществува!");
 
-            if (_context.Requests.FirstOrDefault(r => r.Restaurant == restaurant && r.SentOn.AddDays(7) < DateTime.UtcNow) == null) return BadRequest("Вие вече сте изпратили заявка към този ресторант!");
+            if (_context.Requests.FirstOrDefault(r => r.Sender == employeeProfile && r.Restaurant == restaurant && r.SentOn.AddDays(7) > DateTime.UtcNow) != null) return BadRequest("Вие вече сте изпратили заявка към този ресторант!");
             if (restaurant.EmployeesRestaurants.FirstOrDefault(er => er.Employee == employee && er.EndedOn == null) != null) return BadRequest("Вие работите в този ресторант!");
             
             var manager = restaurant.Manager;
