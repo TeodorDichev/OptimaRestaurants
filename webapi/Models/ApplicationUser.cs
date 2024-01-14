89cc66b54f1e07068b76aae4d3e9a4012cac690b
Teodor Dichev <teodichev@gmail.com>
2024-01-14 18:42:29 +0200

    I forgot to do one migration

--- webapi/Models/ApplicationUser.cs
+++ webapi/Models/ApplicationUser.cs
@@ -9,6 +9,8 @@ namespace webapi.Models
         public required string FirstName { get; set; }
         [MaxLength(50)]
         public required string LastName { get; set; }
+        [MaxLength(10), MinLength(10)]
+        public override string? PhoneNumber { get => base.PhoneNumber; set => base.PhoneNumber = value; }
         public string? ProfilePicturePath { get; set; }
         public DateOnly DateCreated { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     }
