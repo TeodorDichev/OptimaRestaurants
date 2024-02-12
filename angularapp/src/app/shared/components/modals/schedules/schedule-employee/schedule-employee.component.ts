4e9b5298fb5947ba2d34531c8865496e25134275
kordiP <nasikordov@gmail.com>
2024-02-12 12:29:16 +0200

    prgrs

--- angularapp/src/app/shared/components/modals/schedules/schedule-employee/schedule-employee.component.ts
+++ angularapp/src/app/shared/components/modals/schedules/schedule-employee/schedule-employee.component.ts
@@ -16,6 +16,10 @@ export class ScheduleEmployeeComponent implements OnInit {
   firstDayOfMonth: number = 0; // gets what the first date's weekday is
   lastDayOfMonth: number = 0; // gets what the last date's weekday is
 
+
+  week5LastDay: number = 0;
+  week6LastDay: number = 0;
+
   weekdays = ['Нед', 'Пон', 'Вто', 'Сря', 'Чет', 'Пет', 'Съб'];
 
   constructor(private emplopyeeService: EmployeeService,
@@ -30,6 +34,7 @@ export class ScheduleEmployeeComponent implements OnInit {
     this.getDaysCountInCurrentMonth();
     this.getFirstWeekDay();
     this.getLastWeekDay();
+    this.lastWeeksNumbers();
   }
 
   private getEmployee() {
@@ -70,18 +75,14 @@ export class ScheduleEmployeeComponent implements OnInit {
     this.lastDayOfMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 0).getDay() + 1;
   }
 
-  lastWeeksNumbers(weekNumber: number) {
-    const weekLastDate = (weekNumber) * 7 - this.firstDayOfMonth;
-    const weekFirstDate = (weekNumber) * 6 + 1;
-    if (weekFirstDate > this.daysInCurrentMonth) {
-      return 0;
-    }
-    if (weekLastDate < this.daysInCurrentMonth) {
-      return this.lastDayOfMonth;
-    }
-    if (weekLastDate > this.daysInCurrentMonth) {
-      return this.lastDayOfMonth;
-    }
-    return -1;
+  lastWeeksNumbers() { 
+    const week5FirstDay = 7 - this.firstDayOfMonth + (5 - 2) * 7 + 1;
+    const week6FirstDay = 7 - this.firstDayOfMonth + (6 - 2) * 7 + 1;
+
+    this.week5LastDay = 6 + week5FirstDay;
+    this.week6LastDay = 6 + week6FirstDay;
+
+    console.log('5 ' + week5FirstDay + ' ' + this.week5LastDay);
+    console.log('6 ' + week6FirstDay + ' ' + this.week6LastDay);
   }
 }
